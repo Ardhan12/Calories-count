98eb3bdd478841bcf6f71716425cee1b3db9c2eb
Ardhan12 <arief.ramadhan43@gmail.com>
2023-01-19 10:41:57 +0700

    Initial Commit

---
+++ CRUD-App/CRUD_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUD_AppApp.swift
+//  CRUD-App
+//
+//  Created by Arief Ramadhan on 19/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CRUD_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

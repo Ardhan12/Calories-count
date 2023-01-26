53f0309ce793d0ceedf685be03bf1d277a675b9b
Ardhan12 <arief.ramadhan43@gmail.com>
2023-01-26 22:55:45 +0700

    create profile page

---
+++ CRUD-App/View/Profile.swift
@@ -0,0 +1,22 @@
+//
+//  Profile.swift
+//  CRUD-App
+//
+//  Created by Arief Ramadhan on 26/01/23.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        VStack{
+            Text("ini profile page")
+        }
+    }
+}
+
+struct Profile_Previews: PreviewProvider {
+    static var previews: some View {
+        Profile()
+    }
+}
